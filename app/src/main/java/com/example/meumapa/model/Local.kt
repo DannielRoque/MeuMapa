1c51d56719fff3718816713151aecf96c79a51df
unknown <daniel.atdrroque05@gmail.com>
2020-01-13 18:01:56 -0300

    update

---
+++ app/src/main/java/com/example/meumapa/model/Local.kt
@@ -0,0 +1,10 @@
+package com.example.meumapa.model
+
+class Local(
+    val id : Int,
+    val imagem : String,
+    val descricao : String,
+    val telefone : String,
+    val observacao : String,
+    val nota : Int
+)
